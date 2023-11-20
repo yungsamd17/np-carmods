6fb34618fab8e9bd1c85942819b69218694d629e
yungsamd17 <yungsamd@proton.me>
2023-11-20 08:45:48 +0100

    🔧Remove unused code

--- src/js/intElements.js
+++ src/js/intElements.js
@@ -1,23 +1,3 @@
-/* When the user clicks on the button, 
-toggle between hiding and showing the dropdown content */
-function myFunction() {
-  document.getElementById("myDropdown").classList.toggle("show");
-}
-
-// Close the dropdown if the user clicks outside of it
-window.onclick = function(event) {
-  if (!event.target.matches('.pastebinButton')) {
-    var dropdowns = document.getElementsByClassName("dropdown-content");
-    var i;
-    for (i = 0; i < dropdowns.length; i++) {
-      var openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains('show')) {
-        openDropdown.classList.remove('show');
-      }
-    }
-  }
-}
-
 // Back to the top Button
 let mybutton = document.getElementById("topBtn");
 window.onscroll = function() {
